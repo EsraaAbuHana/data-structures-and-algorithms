159a96f67ad88d782675d2d219a34111a34661de
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-04-18 21:36:15 +0300

    fix CC-10

---
+++ Data-Structures/stacksandqueues/lib/src/main/java/stacksandqueues/Queue.java
@@ -0,0 +1,69 @@
+package stacksandqueues;
+
+public class Queue<T> {
+  Node front;
+  Node rear;
+  public Queue() {
+
+  }
+  public Queue(T value) {
+    this.front = new Node(value);
+    this.rear = this.front;
+  }
+
+
+
+  public void enqueue(T value) {
+    try {
+      if(rear==null){
+        front=new Node(value);
+        rear=front;
+      }else {
+        this.rear.next=new Node(value);
+        this.rear=this.rear.next;
+      }
+    }catch (Exception ex){
+      System.out.println(ex);
+    }
+  }
+
+  public Object dequeue() {
+    Node deletedNode=front;
+    try {
+      if(isEmpty()){
+        new NullPointerException();
+      }else {
+        this.front=front.next;
+        deletedNode.next=null;
+      }
+    }catch (Exception ex){
+      System.out.println(ex);
+
+    }
+//    return deletedNode.value;
+
+    return deletedNode;
+  }
+  public Object peek(){
+    try {
+      if (isEmpty()){
+        return new  NullPointerException();
+      }else{
+        return front.value;
+      }
+    }catch (Exception ex){
+      return ex;
+    }
+  }
+
+  public boolean isEmpty() {
+    return front == null;
+  }
+  @Override
+  public String toString() {
+    return "Queue{" +
+      "front=" + front +
+      ", rear=" + rear +
+      '}';
+  }
+}
