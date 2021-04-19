96d4a8131d56696d44ec024d099d7ceff220585d
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-04-19 20:53:06 +0300

    dequeue not fix

---
+++ challenges/utilities/lib/src/main/java/utilities/Node.java
@@ -0,0 +1,19 @@
+package utilities;
+
+  public class Node<T> {
+    T value;
+    public Node next;
+
+    public Node(T value) {
+      this.value = value;
+    }
+
+    @Override
+    public String toString() {
+      return "Node{" +
+        "value=" + value +
+        ", next=" + next +
+        '}';
+    }
+  }
+
