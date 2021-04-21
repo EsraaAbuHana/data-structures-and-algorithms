5e68e6b6dc24e15826347b6fb73aec115a62339b
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-04-21 03:27:24 +0300

    func

--- challenges/utilities/lib/src/main/java/utilities/MultiBracketValidation.java
+++ challenges/utilities/lib/src/main/java/utilities/MultiBracketValidation.java
@@ -1,4 +1,65 @@
 package utilities;
+import java.lang.*;
+import java.util.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//  {}	TRUE
+//  {}(){}	TRUE
+//    ()[[Extra Characters]]	TRUE
+//    (){}[[]]	TRUE
+//  {}{Code}[Fellows](())	TRUE
+//[({}]	FALSE
+//    (](	FALSE
+//  {(})	FALSE
 public class MultiBracketValidation {
+
+
+static boolean multiBracketValidation(String str)
+{
+  ArrayList<Character> chars=new ArrayList<Character>();
+  for(int i=0;i<str.length();i++)
+  {
+    if (str.charAt(i)=='{'||str.charAt(i)=='}'||str.charAt(i)=='('||str.charAt(i)==')'||str.charAt(i)=='['||str.charAt(i)==']'){
+      chars.add(str.charAt(i));}
+  }
+  System.out.println(chars);
+  if (chars.size()==1)return false;
+  for (int i = 0; i < chars.size()-1; i++)
+  {
+    char nextToC = chars.get(i+1);
+    char c = chars.get(i);
+
+//    System.out.println(c);
+    if (c == '{' || c == '(' || c == '[')
+    {
+      if(nextToC=='}'){
+        if (c == '(' || c == '['){
+          return false;
+        }
+      }
+      if(nextToC==')'){
+        if (c == '{' || c == '['){
+          return false;
+        }
+      }
+      if(nextToC==']'){
+        if (c == '{' || c == '('){
+          return false;
+        }
+      }
+    }
+
+  }
+  return true;
+}
+
+  public static void main(String[] args)
+  {
+
+//    String str = "{dxs}ljkh])";
+    String str = "[()}]";
+      System.out.println(multiBracketValidation(str));
+  }
 }
