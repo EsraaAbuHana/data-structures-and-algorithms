cd7fb0b1c45881a3bdad5d578e5f9bde0a894933
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-04-25 23:49:38 +0300

    working ....

---
+++ challenges/find-maximum-value/app/src/main/java/find/maximum/value/BinaryTree.java
@@ -0,0 +1,84 @@
+package find.maximum.value;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+
+public class BinaryTree<T> {
+  Node root;
+
+  ArrayList<T> tree = new ArrayList<T>();
+  //preorder
+  //  Pre-order: root >> left >> right
+
+  public ArrayList<Object> preOrder(Node root) {
+    ArrayList<Object> output=new ArrayList<Object>();
+    try {
+
+      if (root == null) {
+        return output;
+      } else {
+        output.add(root.value);
+        if (root.left != null)
+          output.addAll(preOrder(root.left)) ;
+
+        if (root.right != null)
+          output.addAll(preOrder(root.right)) ;
+      }
+
+    } catch (Exception ex) {
+      System.out.println(ex);
+    }
+
+    return output;
+  }
+
+  //inOrder
+  //  In-order: left >> root >> right
+  public ArrayList<Object> inOrder(Node root) {
+    ArrayList<Object> output=new ArrayList<Object>();
+    try {
+
+      if (root == null) {
+        return output;
+      } else {
+        if (root.left != null)
+          output.addAll(inOrder(root.left)) ;
+        output.add(root.value);
+
+        if (root.right != null)
+          output.addAll(inOrder(root.right)) ;
+      }
+
+    } catch (Exception ex) {
+      System.out.println(ex);
+    }
+
+    return output;
+  }
+
+  //  //postOrder
+////  Post-order: left >> right >> root
+  public ArrayList<Object> postOrder(Node root) {
+    ArrayList<Object> output=new ArrayList<Object>();
+    try {
+
+      if (root == null) {
+        return output;
+      } else {
+        if (root.left != null)
+          output.addAll(postOrder(root.left)) ;
+
+        if (root.right != null)
+          output.addAll(postOrder(root.right)) ;
+        output.add(root.value);
+      }
+
+    } catch (Exception ex) {
+      System.out.println(ex);
+    }
+
+    return output;
+  }
+
+}
