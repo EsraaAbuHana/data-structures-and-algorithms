f77c1705452dbda6a8a1a7574a5be204265020e8
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-20 19:54:07 +0300

    done

---
+++ Data-Structures/hashtable/app/src/main/java/linkedList/LinkedList.java
@@ -0,0 +1,198 @@
+package linkedList;
+
+
+public class LinkedList {
+  public static void main(String[] args) {
+    LinkedList linkedList = new LinkedList();
+//    linkedList.insert(7);
+//    System.out.println(linkedList.toString());
+//    System.out.println(linkedList.includes(7));
+//    System.out.println(linkedList.includes(8));
+//    linkedList.insert(8);
+//    System.out.println(linkedList.toString());
+//    System.out.println(linkedList.includes(8));
+//    linkedList.append(5);
+//    linkedList.append(6);
+//    linkedList.insertBefore(6, 9);
+//    linkedList.insertAfter(5, 2);
+//    //linkedList.findK(2);
+//    System.out.println("Node of K :" + linkedList.findNodeK(1));
+//    //add new LinkedList
+//    LinkedList linkedList2 = new LinkedList();
+//    linkedList2.insert(20);
+//    linkedList2.append(30);
+//    linkedList2.append(40);
+//
+//    System.out.println("linkedList2 : " + linkedList2.toString());
+//    zipLists(linkedList, linkedList2);
+//    System.out.println(linkedList.toString());
+
+
+  }
+
+
+  public Node head;
+
+  public LinkedList() {
+    this.head = null;
+  }
+
+  public void insert(int val) {
+    Node newNode = new Node(val);
+    if (head == null) {
+      head = newNode;
+    } else {
+      newNode.next = head;
+      head = newNode;
+
+    }
+  }
+
+  //  Define a method called includes which takes any value as an argument
+//  and returns a boolean result depending on whether that value exists
+//  as a Node’s value somewhere within the list.
+//  public boolean includes(int val) {
+//    Node current = this.head;
+//    while (current != null) {
+//      if (current.val == val) {
+//        return true;
+//      } else {
+//        current = current.next;
+//      }
+//    }
+
+//    return false;
+//  }
+
+  //  Define a method called toString (or __str__ in Python) which takes
+//  in no arguments and returns a string representing all the values in
+//  the Linked List, formatted as:   "{ a } -> { b } -> { c } -> NULL"
+  public String toString() {
+    Node curent = this.head;
+    String linkedListValues = "";
+    while (curent != null) {
+
+
+      linkedListValues = linkedListValues + "{" + curent.val + "}->";
+
+      curent = curent.next;
+
+    }
+    return linkedListValues + "Null";
+  }
+
+
+  //.append(value)
+//  Input                        	Args	                    Output
+//  head -> [1] -> [3] -> [2] -> X	5	head -> [1] -> [3] -> [2] -> [5] -> X
+//
+  public void append(Object val) {
+    Node appendedNode = new Node(val);
+//    String linkedListValues="";
+    Node currentNode = head;
+    if (head == null) {
+      head = appendedNode;
+    } else {
+      while (currentNode.next != null) {
+
+        currentNode = currentNode.next;
+      }
+      currentNode.next = appendedNode;
+    }
+  }
+
+  //  .insertBefore(value, newVal)
+//  Input	Args	Output
+//  head -> [1] -> [3] -> [2] -> X	3, 5	head -> [1] -> [5] -> [3] -> [2] -> X
+//  head -> [1] -> [3] -> [2] -> X	4, 5	Exception
+//  public void insertBefore(int val, int newVal) {
+//    Node appendedNode = new Node(newVal);
+//
+////    String linkedListValues="";
+//
+//    Node currentNode = head;
+//    if (head != null && head.val == val) {
+//      appendedNode.next = head;
+//      head = appendedNode;
+//    } else {
+//      while (currentNode.next != null) {
+//        if (currentNode.next.val == val) {
+//          appendedNode.next = currentNode.next;
+//          currentNode.next = appendedNode;
+//          break;
+//        }
+//        currentNode = currentNode.next;
+//      }
+//    }
+//  }
+
+//  public void insertAfter(int val, int newVal) {
+//
+//    Node current = this.head;
+//    Node newNode = new Node(newVal);
+//    while (current != null) {
+//      if (current.val == val) {
+//        newNode.next = current.next;
+//        current.next = newNode;
+//        break;
+//      }
+//      current = current.next;
+//    }
+//
+//  }
+
+//  //k is the index from the last
+//  public int findNodeK(int k) {
+//    int counter = 0;
+//    int counterReverse = 0;
+//
+//    Node current = this.head;
+//
+//    while (current.next != null) {
+//      current = current.next;
+//      counter++;
+//    }
+//    System.out.println("counter : " + counter);//5
+//    counterReverse = counter - k;
+//    counter = 0;
+//    current = this.head;
+//    System.out.println(counter);
+//    System.out.println(counterReverse);
+//    while (current.next != null) {
+//      if (counterReverse == counter) {
+//        break;
+//      }
+//      current = current.next;
+//      counter++;
+//    }
+//    return current.val;
+//  }
+
+  public static LinkedList zipLists(LinkedList firstList, LinkedList secondList) {
+    Node firstCurrent = firstList.head;
+    Node secondCurrent = secondList.head;
+    Node firstNext;
+    Node secondNext;
+    if (firstCurrent == null) {
+      return secondList;
+    }
+    if (secondCurrent == null) {
+      return firstList;
+    }
+    while (firstCurrent != null && secondCurrent != null) {
+      firstNext = firstCurrent.next;
+      secondNext = secondCurrent.next;
+      firstCurrent.next = secondCurrent;
+      firstCurrent = firstNext;
+      if (firstCurrent == null) {
+        break;
+      }
+      secondCurrent.next = firstCurrent;
+      secondCurrent = secondNext;
+      if (secondCurrent == null) {
+        break;
+      }
+    }
+    return firstList;
+  }
+}
---
+++ Data-Structures/hashtable/app/src/test/java/hashtable/HashTableTest.java
@@ -0,0 +1,38 @@
+package hashtable;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HashTableTest {
+  @Test public void testHashTable(){
+    Hashtable claasUnderTest= new Hashtable();
+    assertEquals(true,claasUnderTest instanceof Hashtable);
+  }
+  @Test public void testGetHash(){
+    Hashtable claasUnderTest= new Hashtable();
+    assertEquals(876,claasUnderTest.hash("groot"));
+    assertEquals(602,claasUnderTest.hash(96));
+  }
+  @Test public void testAdd(){
+    Hashtable claasUnderTest= new Hashtable();
+    claasUnderTest.add("groot","esraa");
+    assertEquals("{Node{key=groot, value=esraa}}->Null", claasUnderTest.getHashNodesList()[876].toString());
+    claasUnderTest.add("groot","fox");
+    assertEquals("{Node{key=groot, value=esraa}}->{Node{key=groot, value=fox}}->Null", claasUnderTest.getHashNodesList()[876].toString());
+
+  }
+  @Test public void testGet(){
+    Hashtable claasUnderTest= new Hashtable();
+    claasUnderTest.add("groot","esraa");
+    assertEquals("esraa", claasUnderTest.get("groot"));
+    assertEquals("this key is not found", claasUnderTest.get("nebula"));
+
+  }
+  @Test public void testContains(){
+    Hashtable claasUnderTest= new Hashtable();
+    claasUnderTest.add("groot","esraa");
+    assertEquals(876, claasUnderTest.contains("groot"));
+    assertEquals(-1, claasUnderTest.contains("esraa"));
+
+
+  }
+}
