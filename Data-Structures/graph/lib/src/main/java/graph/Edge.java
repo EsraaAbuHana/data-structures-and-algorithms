c3bfa2d60c01956d92ec0cf95a46c2cff43b4256
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-26 20:24:51 +0300

    initial

---
+++ Data-Structures/graph/lib/src/main/java/graph/Edge.java
@@ -0,0 +1,29 @@
+package graph;
+
+public class Edge {
+  private Vertex vertex;
+  private Object wight;
+  public Edge(Vertex vertex, Object wight) {
+    this.vertex=vertex;
+this.wight=wight;
+  }
+  public Edge(Vertex vertex) {
+    this.vertex=vertex;
+  }
+
+  public Vertex getVertex() {
+    return vertex;
+  }
+
+  public void setVertex(Vertex vertex) {
+    this.vertex = vertex;
+  }
+
+  public Object getWight() {
+    return wight;
+  }
+
+  public void setWight(Object wight) {
+    this.wight = wight;
+  }
+}
---
+++ Data-Structures/graph/lib/src/main/java/graph/Graph.java
@@ -0,0 +1,55 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph {
+  private HashMap <Vertex,ArrayList>vertices = new HashMap<Vertex,ArrayList>();
+  private int size;
+  public Graph() {
+  }
+
+  public Vertex Node(Vertex vertex){
+    vertices.put(vertex,new ArrayList());
+size++;
+return vertex;
+  }
+public void addEdge(Vertex start,Vertex end,int wight){
+   if(this.vertices.containsKey(start)&&this.vertices.containsKey(end)){
+     ArrayList edges=this.vertices.get(start);
+     edges.add(new Edge(end,wight));
+     this.vertices.put(start,edges);
+   }
+}
+  public HashMap<Vertex, ArrayList> getVertices() {
+    return vertices;
+  }
+public List getNodes(){
+   List<Vertex> nodes=new ArrayList<>();
+ nodes.forEach((vertex) ->nodes.add(vertex));
+//   vertices.entrySet()
+//    .forEach((entry) -> nodes.add(entry.getKey()));
+   return nodes;
+}
+public List getNeighbors(Vertex vertex){
+    if(vertices.containsKey(vertex)){
+      return vertices.get(vertex);
+    }
+   return new ArrayList();
+}
+
+  public void setVertices(HashMap<Vertex, ArrayList> vertices) {
+    this.vertices = vertices;
+  }
+  public int getSize() {
+    return size;
+  }
+
+  public void setSize(int size) {
+    this.size = size;
+  }
+}
+
+
