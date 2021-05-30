6abd3166f0fc77a6e98ef8dc3c16c49d1d4df0ec
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-30 03:52:19 +0300

    add test

--- Data-Structures/hashtable/app/src/main/java/TreeIntersection/TreeIntersection.java
+++ Data-Structures/hashtable/app/src/main/java/TreeIntersection/TreeIntersection.java
@@ -1,17 +1,22 @@
 package TreeIntersection;
 
+
+import hashtable.Hashtable;
 import Tree.BinarySearchTree;
 import Tree.BinaryTree;
 import Tree.NodeTree;
-import hashtable.Hashtable;
+import java.util.ArrayList;
 
 public class TreeIntersection {
+  private static Hashtable output=new Hashtable();
+
   public static void main(String[] args) {
 
     BinarySearchTree first=new BinarySearchTree();
     BinarySearchTree second=new BinarySearchTree();
     first.add(5);
     first.add(1);
+    first.add(4);
     first.add(7);
     first.add(11);
     second.add(10);
@@ -19,7 +24,8 @@ public class TreeIntersection {
     second.add(8);
     second.add(7);
 
-    Hashtable hashtable=treeIntersection(first,second);
+    ArrayList common=treeIntersection(first,second);
+    System.out.println(common);
 //    System.out.println(hashtable.hash(7));
 //
 //    for (int i = 0; i < treeIntersection(first,second).getHashNodesList().length; i++) {
@@ -30,16 +36,54 @@ public class TreeIntersection {
 //        }
     }
 //    System.out.println();
-
-public static Hashtable treeIntersection(BinarySearchTree first,BinarySearchTree second){
-  Hashtable common=new Hashtable();
-  if(first.root==null||second.root==null){return null;}
-  for (int i = 0; i < first.inOrder(first.root).size(); i++) {
-    if (second.contains(i)){
-      common.add(i,i);
-    }
-  }
-  System.out.println(common);
-return common;
+public static ArrayList treeIntersection(BinarySearchTree first,BinarySearchTree second){
+output=new Hashtable();
+output=addToHashTable(first.root);
+ return checkCommon(second.root);
 }
+public static ArrayList checkCommon(NodeTree root){
+ArrayList arrayOutput=new ArrayList();
+  try {
+    if (root == null) {
+      return arrayOutput;
+    } else {
+      if (root.left != null)
+        arrayOutput.addAll(checkCommon(root.left)) ;
+      if (output.contains(root.value)){
+      arrayOutput.add(root.value);}
+      if (root.right != null)
+        arrayOutput.addAll(checkCommon(root.right)) ;
+    }
+  } catch (Exception ex) {
+    System.out.println(ex);
+  }
+  return arrayOutput;
+}
+  public static Hashtable addToHashTable(NodeTree root) {
+    try {
+      if (root == null) {
+        return output;
+      } else {
+        if (root.left != null)
+          addToHashTable(root.left) ;
+        output.add(root.value, root.value);
+        if (root.right != null)
+          addToHashTable(root.right) ;
+      }
+    } catch (Exception ex) {
+      System.out.println(ex);
+    }
+    return output;
+  }
+//public static Hashtable treeIntersection(BinarySearchTree first,BinarySearchTree second){
+//  Hashtable common=new Hashtable();
+//  if(first.root==null||second.root==null){return null;}
+//  for (int i = 0; i < first.inOrder(first.root).size(); i++) {
+//    if (second.contains(i)){
+//      common.add(i,i);
+//    }
+//  }
+//  System.out.println(common);
+//return common;
+//}
 }
---
+++ Data-Structures/hashtable/app/src/test/java/hashtable/TreeIntersectionTest.java
@@ -0,0 +1,17 @@
+package hashtable;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TreeIntersectionTest {
+  @Test
+  public void testRepeatedWord() {
+    RepeatedWord classUnderTest1 = new RepeatedWord();
+//    String string="It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...";
+    String string="Once upon a time, there was a brave princess who..."	;
+//    String string="a s d f g h j r  u i k r"	;
+    System.out.println("the  most repeated one is : "+classUnderTest1.repeatedWord(string));
+    assertTrue(true);
+  }
+}
