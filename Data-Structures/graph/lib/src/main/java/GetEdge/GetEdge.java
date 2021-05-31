ffd19fcf34dd376a3a7e9067c55ea63762b2cdd0
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-31 23:57:31 +0300

    not yet

--- Data-Structures/graph/lib/src/main/java/GetEdge/GetEdge.java
+++ Data-Structures/graph/lib/src/main/java/GetEdge/GetEdge.java
@@ -1,7 +1,12 @@
 package GetEdge;
 
+import graph.Edge;
 import graph.Graph;
 import graph.Vertex;
+import stacksandqueues.Node;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GetEdge {
   public static void main(String[] args) {
@@ -28,16 +33,43 @@ public class GetEdge {
     graph.addEdge(Narnia,Naboo,250);
     System.out.println(graph.getNeighbors(Pandora));
     System.out.println(graph.getNeighbors(Arendelle));
+    System.out.println(graph.getVertices());
+    System.out.println(graph.getNodes());
+//    System.out.println(graph.);
+    String[] cityNames={"Metroville","Pandora"};
+    System.out.println("the cost is : "+getEdge(graph,cityNames));
 
   }
-//  [Metroville, Pandora, ]	True, $82
+//  [Metroville, Pandora]	True, $82
 //[Arendelle, New Monstropolis, Naboo]	True, $115
 //[Naboo, Pandora]	False, $0
 //[Narnia, Arendelle, Naboo]	False, $
   public static int getEdge(Graph graph,String[]cityNames){
     int cost=0;
-//Vertex locCity=graph.getVertices()[0];
+Vertex locCity=null;
 //graph.getNeighbors(locCity);
+    for (Vertex city : (ArrayList<Vertex>) graph.getNodes()) {
+      if (city.getValue()== cityNames[0]) {
+        locCity = city;
+        System.out.println(locCity);
+      }
+    }
+
+    for (int i = 1; i < cityNames.length; i++) {
+      for (Edge edge : (List<Edge>) graph.getNeighbors(locCity)) {
+        if (edge .getVertex().getValue()== cityNames[i]) {
+          System.out.println("*********************"+graph.getNeighbors(locCity));
+          System.out.println(edge.getVertex());
+          locCity = edge.getVertex();
+          cost=cost+(int)edge.getWight();
+
+
+          break;
+        }
+      }
+
+
+    }
 return cost;
   }
 
